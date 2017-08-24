0c6ed628473d99b022424d1c6b62982fd7859e58
dvjyothsna <jyothsnadonapati@Skatkam-598.local>
2017-08-24 08:58:00 -0700

    DRILL-4286: Graceful shutdown of drillbit
    closes #921

--- exec/java-exec/src/main/java/org/apache/drill/exec/ops/QueryContext.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/ops/QueryContext.java
@@ -204,6 +204,10 @@ public class QueryContext implements AutoCloseable, OptimizerRulesContext, Schem
     return drillbitContext.getBits();
   }
 
+  public Collection<DrillbitEndpoint> getOnlineEndpoints() {
+    return drillbitContext.getBits();
+  }
+
   public DrillConfig getConfig() {
     return drillbitContext.getConfig();
   }
